69417c11cf1458e66f1303444a25aa189000f07b
idsubhasish <95999921+idsubhasish@users.noreply.github.com>
2021-12-18 23:12:43 +0530

    Update mirror_status.py

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -16,7 +16,7 @@ def mirror_status(update, context):
             currentTime = get_readable_time(time.time() - botStartTime)
             total, used, free = shutil.disk_usage('.')
             free = get_readable_file_size(free)
-            message = 'No Active Downloads !\n___________________________'
+            message = '<b>No Active Downloads ! 😕</b>\n___________________________'
             message += f"\n<b>CPU:</b> {psutil.cpu_percent()}% | <b>FREE:</b> {free}" \
                        f"\n<b>RAM:</b> {psutil.virtual_memory().percent}% | <b>UPTIME:</b> {currentTime}" 
             reply_message = sendMessage(message, context.bot, update)
